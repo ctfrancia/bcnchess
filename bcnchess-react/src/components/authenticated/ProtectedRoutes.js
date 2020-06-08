e387d29e141adbf76645054659c8c7afbc5d1687
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-09 03:36:44 +0530

    Add a route for tournament details

--- bcnchess-react/src/components/authenticated/ProtectedRoutes.js
+++ bcnchess-react/src/components/authenticated/ProtectedRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './home/Home'
 import About from './about/About'
+import TournamentDetails from './home/TournamentDetails';
 
 const ProtectedRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const ProtectedRoutes = () => {
       <Route exact path='/'><Redirect to='/home' /></Route>
       <Route path='/home'><Home /></Route>
       <Route path='/about'><About /></Route>
+      <Route path='/tournament/:tournamentId'><TournamentDetails /></Route>
     </Switch>
   );
 };
