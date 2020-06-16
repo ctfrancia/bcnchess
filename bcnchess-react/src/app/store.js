30ae29ecaffffc03ff6da50c08e95a5042196f3b
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-16 14:10:33 +0530

    Add createTournamentFormData state

--- bcnchess-react/src/app/store.js
+++ bcnchess-react/src/app/store.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import userReducer from './userSlice';
 import tournamentsReducer from './tournamentsSlice';
+import createTournamentFormDataReducer from './createTournamentSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
-    tournaments: tournamentsReducer
+    tournaments: tournamentsReducer,
+    createTournamentFormData: createTournamentFormDataReducer
   },
-  middleware: [logger]
+  middleware: [...getDefaultMiddleware(), logger]
 });
