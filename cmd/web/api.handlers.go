7eb015d1d8553e9035623a7c6faac235c2cadff2
Christian Francia <ctfrancia@gmail.com>
2020-11-20 19:11:05 +0100

    #15 wip

--- cmd/web/api.handlers.go
+++ cmd/web/api.handlers.go
@@ -2,12 +2,60 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
+
+	"github.com/ctfrancia/bcnchess/pkg/forms"
+	"github.com/ctfrancia/bcnchess/pkg/models"
 )
 
 func (app *application) apiRegisterNewUSer(w http.ResponseWriter, r *http.Request) {
 
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("firstName", "email", "password", "lastName", "retypePassword")
+	form.PasswordsMatch(form.Get("password"), form.Get("retypePassword"))
+	form.MaxLength("firstName", 255)
+	form.MaxLength("email", 255)
+	form.MatchesPattern("email", forms.EmailRX)
+	form.MinLength("password", 6)
+	form.MinLength("retypePassword", 6)
+
+	if !form.Valid() {
+		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		return
+	}
+
+	u := &models.User{
+		FirstName:        form.Get("firstName"),
+		LastName:         form.Get("lastName"),
+		Email:            form.Get("email"),
+		Password:         []byte(form.Get("password")),
+		Club:             form.Get("club"),
+		EloStandard:      form.Get("eloStandard"),
+		EloRapid:         form.Get("eloRapid"),
+		LichessUsername:  form.Get("lichessUsername"),
+		ChesscomUsername: form.Get("chesscomUsername"),
+	}
+
+	err = app.users.Insert(u)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", models.ErrEmailAlreadyExists)
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	app.session.Put(r, "flash", "Your signup was successful. Please login")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) apiGetLatestTournaments(w http.ResponseWriter, r *http.Request) {
